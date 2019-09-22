b883ff97079310a01f8e4e9ef79fcce274cc9585
YoungSun Shin <tlsdudtjs128@gmail.com>
2019-09-22 23:23:24 +0900

    #3 코드 구현 by 3110 신영선

--- BeautyForestAgent/FormOpinion.Designer.cs
+++ BeautyForestAgent/FormOpinion.Designer.cs
@@ -33,18 +33,18 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.tlsMenu = new System.Windows.Forms.ToolStrip();
-            this.fontDlg = new System.Windows.Forms.FontDialog();
-            this.colorDlg = new System.Windows.Forms.ColorDialog();
-            this.rgbText = new System.Windows.Forms.RichTextBox();
             this.tsbtnFont = new System.Windows.Forms.ToolStripButton();
             this.tsbtnColor = new System.Windows.Forms.ToolStripButton();
+            this.fontDlg = new System.Windows.Forms.FontDialog();
+            this.colorDlg = new System.Windows.Forms.ColorDialog();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
             this.statusStrip1.SuspendLayout();
             this.tlsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 264);
+            this.button1.Location = new System.Drawing.Point(12, 227);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(464, 46);
@@ -57,9 +57,9 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 319);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 291);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(485, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(516, 22);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
@@ -77,18 +77,9 @@
             this.tsbtnColor});
             this.tlsMenu.Location = new System.Drawing.Point(0, 0);
             this.tlsMenu.Name = "tlsMenu";
-            this.tlsMenu.Size = new System.Drawing.Size(485, 25);
+            this.tlsMenu.Size = new System.Drawing.Size(516, 25);
             this.tlsMenu.TabIndex = 2;
             this.tlsMenu.Text = "toolStrip1";
-            // 
-            // rgbText
-            // 
-            this.rgbText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rgbText.Location = new System.Drawing.Point(0, 25);
-            this.rgbText.Name = "rgbText";
-            this.rgbText.Size = new System.Drawing.Size(485, 294);
-            this.rgbText.TabIndex = 3;
-            this.rgbText.Text = resources.GetString("rgbText.Text");
             // 
             // tsbtnFont
             // 
@@ -98,6 +89,7 @@
             this.tsbtnFont.Name = "tsbtnFont";
             this.tsbtnFont.Size = new System.Drawing.Size(23, 22);
             this.tsbtnFont.Text = "toolStripButton1";
+            this.tsbtnFont.Click += new System.EventHandler(this.TsbtnFont_Click);
             // 
             // tsbtnColor
             // 
@@ -106,14 +98,24 @@
             this.tsbtnColor.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbtnColor.Name = "tsbtnColor";
             this.tsbtnColor.Size = new System.Drawing.Size(23, 22);
-            this.tsbtnColor.Text = "toolStripButton2";
+            this.tsbtnColor.Text = "ㄱ";
+            this.tsbtnColor.Click += new System.EventHandler(this.TsbtnColor_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rtbText.Location = new System.Drawing.Point(0, 25);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(516, 156);
+            this.rtbText.TabIndex = 3;
+            this.rtbText.Text = resources.GetString("rtbText.Text");
             // 
             // FormOpinion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 341);
-            this.Controls.Add(this.rgbText);
+            this.ClientSize = new System.Drawing.Size(516, 313);
+            this.Controls.Add(this.rtbText);
             this.Controls.Add(this.tlsMenu);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.button1);
@@ -139,7 +141,7 @@
         private System.Windows.Forms.ToolStrip tlsMenu;
         private System.Windows.Forms.FontDialog fontDlg;
         private System.Windows.Forms.ColorDialog colorDlg;
-        private System.Windows.Forms.RichTextBox rgbText;
+        private System.Windows.Forms.RichTextBox rtbText;
         private System.Windows.Forms.ToolStripButton tsbtnFont;
         private System.Windows.Forms.ToolStripButton tsbtnColor;
     }
